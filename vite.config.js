3d8ccd53b56bf313fd2dc7ce00e2a3234d66c1d9
julbrn <juliaburenko@yandex.ru>
2025-04-29 17:40:34 +0500

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import ghPages from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tailwind_project/',
   plugins: [
     tailwindcss(),
-    react()],
+    react(),
+    ghPages()],
   server: {
     host: '0.0.0.0',
     port: 5173,
