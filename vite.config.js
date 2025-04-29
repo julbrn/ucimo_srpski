535491e71a657bd7e1123245a64636e4505127db
julbrn <juliaburenko@yandex.ru>
2025-04-29 17:45:10 +0500

    fix: gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import ghPages from 'vite-plugin-gh-pages'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/tailwind_project/',
-  plugins: [
-    tailwindcss(),
-    react(),
-    ghPages()],
+  plugins: [tailwindcss(), react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
