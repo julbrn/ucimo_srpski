5c7eebd8f352effc16ad1bc25a47504bccf35709
julbrn <juliaburenko@yandex.ru>
2025-06-14 10:22:21 +0200

    base link

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/ucimo_srpski/',
   plugins: [tailwindcss(), react()],
   server: {
     host: '0.0.0.0',
